3d1d654c990d4454747a9a3bd947d3dae712b3a8
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 21:51:17 +0500

    ready task 10

--- src/com/company/PrintNumbers10.java
+++ src/com/company/PrintNumbers10.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class PrintNumbers10 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+
+        printStream.println(name);
+
     }
 }
+
+// Task 1.10
