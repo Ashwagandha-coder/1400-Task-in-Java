b26e8a85a9630166d15dd027b23edbb3a6ea75fa
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 00:23:44 +0500

    ready task 14

--- src/com/printNumber/PrintNumbers14.java
+++ src/com/printNumber/PrintNumbers14.java
@@ -1,13 +1,24 @@
 package com.printNumber;
 
+import java.util.Scanner;
+
 public class PrintNumbers14 {
 
     public static void main(String[] args) {
-	int a = 37;
-    int b = 18;
-    int c = 79;
-    System.out.println(a + " " + b + "  " + c);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int mass[] = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            mass[i] = scanner.nextInt();
+        }
+        scanner.close();
+
+        for (int j = 0; j <= 2; j++) {
+            System.out.print(mass[j] + " ");
+        }
     }
 }
 
-// Task 1.1
+// Task 1.14
