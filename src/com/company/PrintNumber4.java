282b2d0f4dab7443f1d8363d9376746c7b12cdbc
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 20:15:57 +0500

    ready task 5

--- src/com/company/PrintNumber4.java
+++ src/com/company/PrintNumber4.java
@@ -3,9 +3,9 @@ package com.company;
 public class PrintNumber4 {
 
     public static void main(String[] args) {
-        int param1 =5;
-        int param2 =10;
-        int param3 =21;
+        int param1 = 5;
+        int param2 = 10;
+        int param3 = 21;
 
 
         System.out.println(param1);
---
+++ src/com/company/PrintNumbers5.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class PrintNumbers5 {
+
+    public static void main(String[] args) {
+
+        int param1 = 1;
+        int param2 = 2;
+
+        System.out.println(1);
+        System.out.println();
+        System.out.println(2);
+    }
+}
