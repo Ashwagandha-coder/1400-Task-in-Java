7f41d6218c723999bf221f1fcbce432c238e172b
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 22:48:35 +0500

    ready task 13

--- src/com/company/PrintNumbers13.java
+++ src/com/company/PrintNumbers13.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class PrintNumbers13 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+
+
+        printStream.println("Следущее число за числом" + " " + number +" " + "число" + " " + (number + 1));
+        printStream.println("Для числа" + " " + number + " " + "преддущее число" + " " + (number - 1));
+
     }
 }
+// Task 1.13
