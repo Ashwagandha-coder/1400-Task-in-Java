70152415a81311ded4f28ebe368de3bcac29d697
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 15:31:03 +0500

    izm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	int a = 37;
+    int b = 18;
+    int c = 22;
+    System.out.println(a + "");
     }
 }
