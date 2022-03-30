030af24082f1399b836bee5a44aedafe11073699
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 00:11:38 +0500

    add 14-17 files

---
+++ src/com/printNumber/PrintNumbers14.java
@@ -0,0 +1,13 @@
+package com.printNumber;
+
+public class PrintNumbers14 {
+
+    public static void main(String[] args) {
+	int a = 37;
+    int b = 18;
+    int c = 79;
+    System.out.println(a + " " + b + "  " + c);
+    }
+}
+
+// Task 1.1
