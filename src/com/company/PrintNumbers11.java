db2097334fea3e413ea74bff18dda8a222b7673f
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 22:14:01 +0500

    ready task 11

--- src/com/company/PrintNumbers11.java
+++ src/com/company/PrintNumbers11.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class PrintNumbers11 {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String FC = scanner.nextLine();
+        String message = "эта команда чемпион";
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+
+        printStream.print(FC);
+        printStream.print(" - ");
+        printStream.print(message);
+
     }
 }
+// Task 1.11
