8dff6e2ebbb78b7366bfbcef3e00fc265f9cd243
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-30 20:09:42 +0500

    ready task 4

--- src/com/company/PrintNumber4.java
+++ src/com/company/PrintNumber4.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class PrintNumber4 {
+
+    public static void main(String[] args) {
+        int param1 =5;
+        int param2 =10;
+        int param3 =21;
+
+
+        System.out.println(param1);
+        System.out.println(param2);
+        System.out.println(param3);
+    }
 }
+
+// Task 1.4
