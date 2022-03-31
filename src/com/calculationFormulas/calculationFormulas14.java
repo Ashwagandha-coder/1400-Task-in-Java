7cfc41f89449b57a6aff11c8e3b0dae8851616c4
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 20:39:02 +0500

    ready task 2.14

--- src/com/calculationFormulas/calculationFormulas14.java
+++ src/com/calculationFormulas/calculationFormulas14.java
@@ -1,4 +1,24 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas14 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int cat1 = scanner.nextInt();
+        int cat2 = scanner.nextInt();
+
+        System.out.println(Pifagor(cat1,cat2));
+    }
+    public static int Pifagor(int cat1,int cat2) {
+
+        int Pifagor = (int) (Math.pow(cat1,2) + Math.pow(cat2,2));
+
+
+        return (int) Math.pow(Pifagor,2);
+    }
 }
+// Task 2.14
