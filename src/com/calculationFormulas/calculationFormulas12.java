568b970238d0a9f44c84a821bc41a7450d638de2
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 20:21:12 +0500

    ready task 2.12

--- src/com/calculationFormulas/calculationFormulas12.java
+++ src/com/calculationFormulas/calculationFormulas12.java
@@ -1,4 +1,29 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas12 {
+
+
+    public static void main(String[] args) {
+
+        int count;
+        int square;
+
+        Scanner scanner = new Scanner(System.in);
+
+        count = scanner.nextInt();
+        square = scanner.nextInt();
+
+        System.out.println(Dencity(count,square));
+
+    }
+
+    public static double Dencity(double count, double square) {
+
+        return count / square;
+
+    }
+
 }
+// Task 2.12
