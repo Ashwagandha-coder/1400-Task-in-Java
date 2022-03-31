5bfaa5835e00050b90512f16b52b7648e1e47a39
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 17:33:51 +0500

    ready task 2.8

--- src/com/calculationFormulas/calculationFormulas8.java
+++ src/com/calculationFormulas/calculationFormulas8.java
@@ -1,4 +1,38 @@
 package com.calculationFormulas;
 
+import org.jetbrains.annotations.Contract;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class calculationFormulas8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int radius = scanner.nextInt();
+
+
+        for (int i = 0; i <= 1; i++) {
+            double[] res = Circle(radius);
+            System.out.println(res[i]);
+        }
+
+    }
+    @Contract(pure = true)
+    public static double[] Circle(int radius) {
+        double pi = 3.14;
+
+        double p = 2 * pi * radius;
+        double s = Math.pow((pi * radius),2);
+
+        double[] result = new double[2];
+        result[0] = p;
+        result[1] = s;
+
+
+        return result;
+    }
 }
+// Task 2.8
