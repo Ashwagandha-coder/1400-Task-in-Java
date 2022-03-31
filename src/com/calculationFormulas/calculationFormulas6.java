d431f4fd2cd975c6b64d80e80aa06f6d3112b773
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 16:53:22 +0500

    ready task 2.6

--- src/com/calculationFormulas/calculationFormulas6.java
+++ src/com/calculationFormulas/calculationFormulas6.java
@@ -1,4 +1,22 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas6 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int EarthRadius = 6350;
+        int h = scanner.nextInt();
+
+        int distance = (int) (Math.pow(Math.pow((EarthRadius + h),2) - Math.pow(EarthRadius,2),0.5));
+
+        System.out.println(distance);
+
+
+
+    }
 }
+// Task 2.6
