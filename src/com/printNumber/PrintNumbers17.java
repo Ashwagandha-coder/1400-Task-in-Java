89da8f28b697ca7b98d904e94f2b585104dba356
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 11:33:35 +0500

    ready task 17 and finish PrintNumber

--- src/com/printNumber/PrintNumbers17.java
+++ src/com/printNumber/PrintNumbers17.java
@@ -1,13 +1,40 @@
 package com.printNumber;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class PrintNumbers17 {
 
     public static void main(String[] args) {
-	int a = 37;
-    int b = 18;
-    int c = 79;
-    System.out.println(a + " " + b + "  " + c);
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        int mass[] = new int[5];
+
+        for (int i = 0; i <= 4; i++) {
+            mass[i] = scanner.nextInt();
+        }
+
+        String weight = "кг";
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+
+
+        printStream.println(2 + " " + weight);
+        System.out.println(13 + " " + 17);
+
+        System.out.println(mass[0] + " " + 1);
+        System.out.println(19 + " " + mass[1]);
+
+        System.out.println(mass[2] + " " + mass[3]);
+        System.out.println(5 + " " + mass[4]);
+
+
+
+
     }
 }
 
-// Task 1.1
+// Task 1.17
