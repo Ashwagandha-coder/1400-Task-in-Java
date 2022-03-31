388856a4209f435985c02fa1578d85fb9f5fa9c7
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 12:39:11 +0500

    ready task 2.1

--- src/com/calculationFormulas/calculationFormulas.java
+++ src/com/calculationFormulas/calculationFormulas.java
@@ -1,4 +1,24 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas {
+
+    public static void main(String[] args) {
+
+       /* Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        */
+
+        int y = 17;
+
+        int Discriminant = (int) (Math.pow(6,2) * (-1) + (4 * 17 * 13));
+
+        int x1 = (int) (6 - Math.pow(Discriminant,(0.5)) / (2 * 17));
+        int x2 = (int) (6 + Math.pow(Discriminant,(0.5)) / (2 * 17));
+
+        System.out.println(x1 + " " + x2);
+
+    }
 }
+// Task 2.1
