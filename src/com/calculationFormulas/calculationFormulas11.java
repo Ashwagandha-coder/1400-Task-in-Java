3be04b8612415da828c6bf54c14e8e67b3ea62a5
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 19:45:52 +0500

    ready task 2.11

--- src/com/calculationFormulas/calculationFormulas11.java
+++ src/com/calculationFormulas/calculationFormulas11.java
@@ -1,4 +1,26 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas11 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+        int v = scanner.nextInt();
+
+        Density(m,v);
+
+    }
+    public static void Density(int m, int v) {
+
+        double p = (m / v);
+
+        System.out.println(p);
+
+    }
+
 }
+// Task 2.11
