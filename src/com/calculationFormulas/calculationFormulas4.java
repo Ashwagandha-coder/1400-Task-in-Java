128e63dfb84fc55b033d63705b44201d3ae1a0d3
Ashwagandha-coder <max.cacz@yandex.ru>
2022-03-31 14:01:25 +0500

    ready task 2.4

--- src/com/calculationFormulas/calculationFormulas4.java
+++ src/com/calculationFormulas/calculationFormulas4.java
@@ -1,4 +1,17 @@
 package com.calculationFormulas;
 
+import java.util.Scanner;
+
 public class calculationFormulas4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int perimitr = a * 4;
+
+        System.out.println(perimitr);
+    }
 }
+// Task 2.4
