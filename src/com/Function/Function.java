daf6efbcc67684a8815f379a89dfb765052426c1
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-01 11:20:36 +0500

    ready task 14.1 and create directory for chapter 14

---
+++ src/com/Function/Function.java
@@ -0,0 +1,22 @@
+package com.Function;
+
+public class Function {
+
+    public static void main(String[] args) {
+
+        String sm = "*";
+
+        PrintSm(sm);
+
+
+    }
+
+    public static void PrintSm(String sm) {
+
+        for (int i = 0; i <= 59; i++) {
+            System.out.print(sm);
+        }
+
+    }
+}
+// Task 14.1
