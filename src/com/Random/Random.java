d804cc44af03572a9eacdee1c0b2d362992d151c
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-01 11:55:04 +0500

    ready task 10.1

---
+++ src/com/Random/Random.java
@@ -0,0 +1,15 @@
+package com.Random;
+
+public class Random {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 7; i++) {
+
+            System.out.println(Math.random() + " ");
+
+        }
+
+    }
+}
+// Task 10.1
