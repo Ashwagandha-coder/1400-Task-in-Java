dd3763475df30879569f8cf625f196ca35603204
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-02 21:41:53 +0500

    ready Task 2.24

--- src/com/calculationFormulas/calculationFormulas24.java
+++ src/com/calculationFormulas/calculationFormulas24.java
@@ -1,4 +1,50 @@
 package com.calculationFormulas;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class calculationFormulas24 {
+
+    public static void main(String[] args) {
+
+
+        Init();
+
+
+    }
+    private static void Init() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+
+        System.out.println(Arrays.toString(Calculate(first,second)));
+
+
+
+    }
+    @Contract(pure = true)
+    private static int @NotNull [] Calculate(int first, int second) {
+
+        int sum = first + second;
+        int raznost = first - second;
+        int proizedenie = first * second;
+        int delenie = first / second;
+        int chastnoe = first % second;
+
+        int[] Result = new int[5];
+        Result[0] = sum;
+        Result[1] = raznost;
+        Result[2] = proizedenie;
+        Result[3] = delenie;
+        Result[4] = chastnoe;
+
+        return Result;
+
+    }
 }
+// Task 2.24
