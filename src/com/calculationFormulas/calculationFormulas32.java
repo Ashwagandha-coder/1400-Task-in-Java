83d6915a1aa97c8a6c37172f31eaa3362af25c63
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-04 00:15:13 +0500

    refactor task 2.32

--- src/com/calculationFormulas/calculationFormulas32.java
+++ src/com/calculationFormulas/calculationFormulas32.java
@@ -3,23 +3,30 @@ package com.calculationFormulas;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class calculationFormulas32 {
 
+
     public static void main(String[] args) {
 
         CalculationPayment calculationPayment = new CalculationPayment();
         calculationPayment.Inited();
+
+
+
     }
+
 }
 
 
 class CalculationPayment {
 
     private int N;
+
 
     protected void Inited() {
 
@@ -32,8 +39,10 @@ class CalculationPayment {
 
         this.N = scanner.nextInt();
 
+
         Print(Payment(monitor, mouse, keyboard, system_unit));
     }
+
     @Contract(pure = true)
     private int @NotNull [] Payment(int monitor, int mouse, int keyboard, int system_unit) {
 
@@ -48,12 +57,15 @@ class CalculationPayment {
         return ResultZ;
 
     }
+
     private void Print(int @NotNull [] ResultZ) {
 
         PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
 
         printStream.println("3 Компьютера:" + " " + ResultZ[0]);
         printStream.println("N Компьютеров" + " " + ResultZ[1]);
+
+
 
 
     }
