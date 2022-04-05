4741acbb224e94ffbbda4451661b7e7844075b35
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-05 12:54:26 +0500

    test2

---
+++ src/com/calculationFormulas/Test2.java
@@ -0,0 +1,24 @@
+package com.calculationFormulas;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+        Gun gun = new Gun();
+        System.out.println(gun.Gua());
+
+
+
+    }
+}
+strictfp class Gun {
+
+    public double Gua() {
+        return 9.4657457567675675675675673423523523532523452352352352352353232342534634634634634654645731142;
+    }
+
+
+
+
+}
+
