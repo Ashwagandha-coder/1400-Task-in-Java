6909786ed64be6397e56e9973cf461d64256172c
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-06 19:44:58 +0500

    test

---
+++ src/com/calculationFormulas/ReadObject.java
@@ -0,0 +1,26 @@
+package com.calculationFormulas;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+public class ReadObject {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Person personZak = new Person("Zak",3);
+        Person personPasha = new Person("Pasha",4);
+
+        FileInputStream fis = new FileInputStream("people.bin");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        //Person personDan = (Person) ois.readObject();
+       // Person personBob = (Person) ois.readObject();
+
+        System.out.println(ois.readObject().toString());
+        System.out.println(ois.readObject().toString());
+
+        ois.close();
+
+    }
+
+}
--- src/com/calculationFormulas/Test4.java
+++ src/com/calculationFormulas/Test4.java
@@ -40,4 +40,11 @@ class Person implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
