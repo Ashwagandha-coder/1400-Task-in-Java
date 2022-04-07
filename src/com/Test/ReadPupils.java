2b2cd830004c0b1b1aada70a15bfa159610f0c45
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-07 10:56:49 +0500

    refactoring last commit

--- src/com/Test/ReadPupils.java
+++ src/com/Test/ReadPupils.java
@@ -9,13 +9,17 @@ public class ReadPupils {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+        ReadObjectMassive();
+
+    }
+    public static void ReadObjectMassive() throws IOException, ClassNotFoundException {
+
         FileInputStream fileInputStream = new FileInputStream("pupils_2.bin");
         ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
 
         Pupils[] pupilsReadObject = (Pupils[]) objectInputStream.readObject();
 
         System.out.println(Arrays.toString(pupilsReadObject));
-
     }
 
 }
