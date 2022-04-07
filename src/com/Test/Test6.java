894da87622cae84d03325a608d63aee4b70e9f16
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-07 10:58:34 +0500

    refactoring write serealizable

--- src/com/Test/Test6.java
+++ src/com/Test/Test6.java
@@ -9,18 +9,20 @@ public class Test6 implements Serializable {
 
     public static void main(String[] args) throws IOException {
 
+        WriteObjectMassive();
+
+
+    }
+    public static void WriteObjectMassive() throws IOException {
+
         Pupils[] pupils123 = new Pupils[] {new Pupils("Rock",1),new Pupils("Don",2),new Pupils("Lion",3)};
 
-        int count = pupils123.length;
-
-        Pupils[][] pupils456 = new Pupils[][]{pupils123};
 
 
         FileOutputStream fileOutputStream = new FileOutputStream("pupils_2.bin");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
 
         objectOutputStream.writeObject(pupils123);
-
 
     }
 }
