2f06af895c9db96930aa714169ed5de4fbb19709
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-08 22:01:00 +0500

    task 2.31 in work

--- src/com/calculationFormulas/calculationFormulas30.java
+++ src/com/calculationFormulas/calculationFormulas30.java
@@ -1,4 +1,25 @@
 package com.calculationFormulas;
 
 public class calculationFormulas30 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+}
+class SquareFour {
+
+
+    private SquareFour() {
+
+
+
+    }
+
+
 }
--- src/com/calculationFormulas/calculationFormulas31.java
+++ src/com/calculationFormulas/calculationFormulas31.java
@@ -1,4 +1,59 @@
 package com.calculationFormulas;
 
 public class calculationFormulas31 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+class PayMentFood implements PayMentTasty {
+
+    private transient int applePrice;
+    private transient int cookiePrice;
+    private transient int sweetPrice;
+
+    private transient int x;
+    private transient int y;
+    private transient int z;
+
+
+
+
+    public PayMentFood(int applePrice, int cookiePrice, int sweetPrice, int x, int y, int z) {
+
+        this.applePrice = applePrice;
+        this.cookiePrice = cookiePrice;
+        this.sweetPrice = sweetPrice;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+    @Override
+    public void PayMentCockie() {
+
+
+
+    }
+
+    @Override
+    public void PayMentApple() {
+
+    }
+
+    @Override
+    public void PayMentSweet() {
+
+    }
+
+    @Override
+    public void Purchases() {
+
+
+
+    }
 }
