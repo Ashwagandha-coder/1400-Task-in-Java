a9c81ffeceecece114ce5ae47fcdf6c3a2761bf9
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-13 12:35:23 +0500

    enum test 2

--- src/com/VariantsAction/Test/Season.java
+++ src/com/VariantsAction/Test/Season.java
@@ -2,7 +2,18 @@ package com.VariantsAction.Test;
 
 public enum Season {
 
-    SUMMER,AUTMN,SPRING,WINTER;
+    SUMMER(28),AUTMN(5),SPRING(13),WINTER(-34);
+
+    private int temperature;
+
+    private Season(int temperature) {
+
+        this.temperature = temperature;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
 
 
 
--- src/com/VariantsAction/Test/TestEnum.java
+++ src/com/VariantsAction/Test/TestEnum.java
@@ -6,12 +6,6 @@ public class TestEnum {
 
         Season season = Season.SPRING;
 
-        switch (season) {
-            case SPRING -> System.out.println("It's Spring");
-            case AUTMN -> System.out.println("It's AUTMN");
-            case SUMMER -> System.out.println("It's Summer");
-            case WINTER -> System.out.println("It's Winter");
-        }
 
     }
 }
