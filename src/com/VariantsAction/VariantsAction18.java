1fba2db0eefc44e631aab5054d95fa5ad6afd9df
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-15 23:43:22 +0500

    add param radius and side

--- src/com/VariantsAction/VariantsAction18.java
+++ src/com/VariantsAction/VariantsAction18.java
@@ -12,10 +12,14 @@ public class VariantsAction18 {
 
     }
 }
-class Size {
+class Size implements SizeCircleSquare {
 
     private transient final int CIRCLE_S;
     private transient final int SQUARE_S;
+
+    private transient int Radius_Circle;
+    private transient int Side_Square;
+   // private transient int Radius_Circle;
 
     public Size(int CIRCLE, int SQUARE) {
         this.CIRCLE_S = CIRCLE;
@@ -23,5 +27,23 @@ class Size {
     }
 
 
+    @Override
+    public void RadiusCircle() {
 
+    }
+
+    @Override
+    public void SideSquare() {
+
+    }
+
+    @Override
+    public void SquareInCircle() {
+
+    }
+
+    @Override
+    public void CircleInSquare() {
+
+    }
 }
