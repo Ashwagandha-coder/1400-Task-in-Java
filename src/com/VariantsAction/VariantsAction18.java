d5aca50bcc0326cde6985d15ef689571ece37141
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-15 23:59:19 +0500

    ready task 4.18

--- src/com/VariantsAction/VariantsAction18.java
+++ src/com/VariantsAction/VariantsAction18.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 
 public class VariantsAction18 {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
 
+        Size size = new Size(scanner.nextInt(), scanner.nextInt());
+
+        size.RadiusCircle();
+        size.SideSquare();
+        size.CircleInSquare();
+        size.SquareInCircle();
 
     }
 }
 class Size implements SizeCircleSquare {
 
-    private transient final int CIRCLE_S;
-    private transient final int SQUARE_S;
+    private transient final double CIRCLE_S;
+    private transient final double SQUARE_S;
 
-    private transient int Radius_Circle;
-    private transient int Side_Square;
+    private transient double Radius_Circle;
+    private transient double Side_Square;
    // private transient int Radius_Circle;
 
     public Size(int CIRCLE, int SQUARE) {
@@ -30,20 +36,35 @@ class Size implements SizeCircleSquare {
     @Override
     public void RadiusCircle() {
 
+        double PI = 3.14;
+        this.Radius_Circle = Math.pow((this.CIRCLE_S / PI), 0.5);
+
     }
 
     @Override
     public void SideSquare() {
+
+        this.Side_Square = Math.pow(this.SQUARE_S, 0.5);
 
     }
 
     @Override
     public void SquareInCircle() {
 
+        if ((Math.pow((this.Radius_Circle), 2)) >= 2 * Math.pow(this.Side_Square, 2))
+            System.out.println(" Yes " + " SquareInCircle");
+        else
+            System.out.println(" No " + " SquareInCircle");
+
     }
 
     @Override
     public void CircleInSquare() {
+
+        if (this.Radius_Circle <= this.Side_Square / 2)
+            System.out.println("Yes" + " CircleInSquare");
+        else
+            System.out.println(" No " + " CircleInSquare");
 
     }
 }
