601ad8f04f7d6c4bde4f1559a58f13e753c462fd
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-15 23:40:57 +0500

    add interface (task 4.18)

---
+++ src/com/VariantsAction/SizeCircleSquare.java
@@ -0,0 +1,14 @@
+package com.VariantsAction;
+
+public interface SizeCircleSquare {
+
+    public void RadiusCircle();
+
+    public void SideSquare();
+
+    public void SquareInCircle();
+
+    public void CircleInSquare();
+
+
+}
---
+++ src/com/VariantsAction/VariantsAction18.java
@@ -0,0 +1,27 @@
+package com.VariantsAction;
+
+import java.util.Scanner;
+
+public class VariantsAction18 {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+class Size {
+
+    private transient final int CIRCLE_S;
+    private transient final int SQUARE_S;
+
+    public Size(int CIRCLE, int SQUARE) {
+        this.CIRCLE_S = CIRCLE;
+        this.SQUARE_S = SQUARE;
+    }
+
+
+
+}
