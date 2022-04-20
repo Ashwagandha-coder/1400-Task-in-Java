93ed0ac5d9444d82b587279c62acba3e559417b5
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-20 19:06:59 +0500

    test task 4.3

--- src/com/VariantsAction/VariantsAction3.java
+++ src/com/VariantsAction/VariantsAction3.java
@@ -1,3 +1,62 @@
 package com.VariantsAction;
 
-public class VariantsAction3 {}
+import java.util.Scanner;
+
+public class VariantsAction3 {
+
+    public static void main(String[] args) {
+
+        Trigonometry3 trigonometry3 = new Trigonometry3();
+
+        trigonometry3.CompareMethod();
+
+    }
+}
+
+
+
+class Trigonometry3 implements payMent3 {
+
+    private transient final int CHISLO;
+
+
+    public Trigonometry3() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.CHISLO = scanner.nextInt();
+
+    }
+
+    public int getCHISLO() {
+        return CHISLO;
+    }
+
+    public void CompareMethod() {
+
+        if (getCHISLO() > 0)
+            doSome1();
+        else
+            doSome2();
+
+
+    }
+
+
+    @Override
+    public void doSome1() { System.out.println(Math.sin(Math.pow(getCHISLO(), 2))); }
+
+    @Override
+    public void doSome2() { System.out.println(1 + (2 * Math.sin(getCHISLO()) * Math.sin(getCHISLO()))); }
+}
+
+
+
+interface payMent3 {
+
+    void doSome1();
+
+    void doSome2();
+
+
+}
