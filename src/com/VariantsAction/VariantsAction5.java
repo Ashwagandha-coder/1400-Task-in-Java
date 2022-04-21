e83a3e3b9089d84eba018e33f915b7789874dd6e
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-21 13:07:40 +0500

    ready task 4.5

--- src/com/VariantsAction/VariantsAction5.java
+++ src/com/VariantsAction/VariantsAction5.java
@@ -1,3 +1,47 @@
 package com.VariantsAction;
 
-public class VariantsAction5 {}
+import java.util.Scanner;
+
+public class VariantsAction5 {
+
+
+    public static void main(String[] args) {
+
+        ifElse5 ifElse5 = new ifElse5(new Read5());
+        ifElse5.Compare5();
+
+    }
+}
+sealed class Read5 permits ifElse5 {
+
+    private transient final int CHISLO;
+
+    public Read5() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.CHISLO = scanner.nextInt();
+
+    }
+
+    public int getCHISLO() {
+        return CHISLO;
+    }
+}
+
+
+final class ifElse5 extends Read5 {
+
+    public ifElse5(Read5 read5) {}
+
+    public void Compare5() {
+
+        if (getCHISLO() > 3)
+            System.out.println("I");
+        else
+            System.out.println("II");
+
+    }
+
+
+}
