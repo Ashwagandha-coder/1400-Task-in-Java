9c593d3c4d4411710bd566b0958fb9a5d81d1ff4
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-21 13:20:43 +0500

    ready task 4.6

--- src/com/VariantsAction/VariantsAction6.java
+++ src/com/VariantsAction/VariantsAction6.java
@@ -1,3 +1,35 @@
 package com.VariantsAction;
 
-public class VariantsAction6 {}
+import java.util.Scanner;
+
+public class VariantsAction6 {
+
+
+    public static void main(String[] args) {
+
+        Read6 read6 = new Read6();
+        read6.SoutValue6();
+
+    }
+}
+
+final class Read6 implements PrintValue6 {
+
+    @Override
+    public void SoutValue6() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextInt());
+        System.out.println(scanner.nextInt() * -1);
+
+    }
+
+}
+
+sealed interface PrintValue6 permits Read6 {
+
+    void SoutValue6();
+
+
+}
