cbddb0e39ec105b6bc3977f4b366e16ef9572f6e
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-21 16:07:41 +0500

    ready task 4.14

--- src/com/VariantsAction/VariantsAction14.java
+++ src/com/VariantsAction/VariantsAction14.java
@@ -1,4 +1,50 @@
 package com.VariantsAction;
 
+import java.util.Scanner;
+
 public class VariantsAction14 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+class CompareValue14 {
+
+    private final ScanningValues14 scanningValues14 = new ScanningValues14();
+
+    public void CompareValues14() {
+
+        // Недоделал
+
+
+    }
+
+
+}
+
+class ScanningValues14 {
+
+    private transient final int R1;
+    private transient final int R2;
+
+    public ScanningValues14() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.R1 = scanner.nextInt();
+        this.R2 = scanner.nextInt();
+
+
+    }
+
+    public int getR1() {
+        return R1;
+    }
+
+    public int getR2() {
+        return R2;
+    }
 }
