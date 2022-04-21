4a660cddc2ab7b8b5553b2395900ef31d34cab2a
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-21 13:30:45 +0500

    ready task 4.9

--- src/com/VariantsAction/VariantsAction7.java
+++ src/com/VariantsAction/VariantsAction7.java
@@ -1,3 +1,4 @@
 package com.VariantsAction;
 
 public class VariantsAction7 {}
+
--- src/com/VariantsAction/VariantsAction9.java
+++ src/com/VariantsAction/VariantsAction9.java
@@ -1,3 +1,37 @@
 package com.VariantsAction;
 
-public class VariantsAction9 {}
+import java.util.Scanner;
+
+public class VariantsAction9 {
+
+
+    public static void main(String[] args) {
+
+        Read9 read9 = new Read9();
+        read9.MaxMinValue();
+
+    }
+}
+
+final class Read9 implements MaxMin {
+
+    @Override
+    public void MaxMinValue() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+
+
+    }
+}
+
+sealed interface MaxMin permits Read9 {
+
+    void MaxMinValue();
+
+}
