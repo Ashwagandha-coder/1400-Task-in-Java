3fb945ac57dd0638f85f10214cc7953c44380056
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-25 23:23:49 +0500

    add functional interfaces

--- src/com/For/Test/LambdaTest.java
+++ src/com/For/Test/LambdaTest.java
@@ -2,26 +2,40 @@ package com.For.Test;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.function.*;
+
 public class LambdaTest {
 
 
     public static void main(String[] args) {
 
-        TwoNumbers twoNumbers = (a, b) -> a + b;
-        System.out.println(twoNumbers.AsInt(2,3));
+        //TwoNumbers twoNumbers = (a, b) -> a + b;
+        sum(2,2,() -> System.out.println("a" + "b"));
+
+
+
+        Predicate<Integer> predicate = (t) -> t%2 == 0;
+        Consumer<Integer> consumer = (t) -> System.out.println(t);
+        Function<Integer,String> function = (x) -> String.valueOf(x);
+        //Supplier<TwoNumbers> supplier;
+        UnaryOperator<Integer> unaryOperator = (x) -> x*x;
+        BinaryOperator<Integer> binaryOperator = (x, y) -> x*y;
+
+
+
     }
 
-    public static int sum(int a, int b, @NotNull TwoNumbers twoNumbers) {
+    public static void sum(int a, int b, @NotNull TwoNumbers twoNumbers) {
 
-       // int c = twoNumbers.AsInt();
-        return a + b;
+
+        twoNumbers.AsInt();
 
     }
 }
-
+@FunctionalInterface
 interface TwoNumbers {
 
-    public abstract int AsInt(int a, int b);
+    public abstract void AsInt();
 
 }
 
