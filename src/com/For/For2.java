0093d7a618ba5c0e992945b7ce2ab10fab345f29
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-28 11:45:50 +0500

    ready task 5.2

--- src/com/For/For2.java
+++ src/com/For/For2.java
@@ -1,4 +1,33 @@
 package com.For;
 
 public class For2 {
+
+    public static void main(String[] args) {
+
+        new PrintNumber100().PrintChislo();
+
+    }
 }
+
+class PrintNumber100  {
+
+   // private transient final byte A = 100;
+
+    Print print = a -> {
+        for (int i = 0; i <= 19; i++)
+            System.out.print(a + " "); };
+
+    public void PrintChislo() {
+
+        print.printNumbers2((byte) 100);
+    }
+}
+
+interface Print {
+
+    void printNumbers2(byte a);
+
+
+}
+
+// task 5.2
