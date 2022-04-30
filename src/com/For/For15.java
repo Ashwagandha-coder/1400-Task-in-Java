30ec2546c4d5fa7735bf41de2e7bccb66b4fea31
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-30 12:36:24 +0500

    ready task 5.15

--- src/com/For/For15.java
+++ src/com/For/For15.java
@@ -1,4 +1,45 @@
 package com.For;
 
+import java.util.Scanner;
+
 public class For15 {
+
+
+    public static void main(String[] args) {
+
+        new PrintNumbers15().doSome15();
+
+
+    }
 }
+
+final class PrintNumbers15 {
+
+    MethodPrint15 methodPrint15 = a -> {
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(a + " " + "x" + " " + i + " " + "=" + " " + a * i);
+            System.out.println();
+        }
+
+    };
+
+    public void doSome15() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        methodPrint15.MethodPrintMultiply15(scanner.nextByte()); }
+
+
+}
+
+
+@FunctionalInterface
+interface MethodPrint15 {
+
+    void MethodPrintMultiply15(byte a);
+
+
+}
+
+// task 5.15
