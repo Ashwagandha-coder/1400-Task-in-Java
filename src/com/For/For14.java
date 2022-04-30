498d26d43ce19fdc30f7717d2b5f83614ca090fb
Ashwagandha-coder <max.cacz@yandex.ru>
2022-04-30 12:15:25 +0500

    ready task 5.14

--- src/com/For/For14.java
+++ src/com/For/For14.java
@@ -1,4 +1,37 @@
 package com.For;
 
 public class For14 {
+
+
+    public static void main(String[] args) {
+
+        new PrintNumbers14().doSome14();
+
+    }
 }
+
+final class PrintNumbers14 {
+
+    MethodPrint14 methodPrint14 = () -> {
+
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(9 + " " + "x" + " " + i + " " + "=" + " " + i * 9);
+            System.out.println();
+        }
+
+    };
+
+    public void doSome14() { methodPrint14.MethodPrintMultiply14(); }
+
+
+}
+
+
+@FunctionalInterface
+interface MethodPrint14 {
+
+    void MethodPrintMultiply14();
+
+}
+
+// task 5.14
