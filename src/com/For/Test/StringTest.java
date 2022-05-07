03bf22c4ba59ef94b0439e7b22be65f9ebff4311
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-07 08:33:33 +0500

    test stringBuilder and method append

---
+++ src/com/For/Test/StringTest.java
@@ -0,0 +1,17 @@
+package com.For.Test;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        String string = "abc";
+        String string1 = new String("cde");
+        string1.toUpperCase();
+        System.out.println(string1.toString());
+
+        StringBuilder stringBuilder = new StringBuilder("Poroh");
+        stringBuilder.append(" in").append(" gun");
+        System.out.println(stringBuilder);
+
+    }
+}
