572713bd606f08b646b997476bfc003f5edfc5b0
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-22 12:18:08 +0500

    ready task 14.3

--- src/com/Function/Function3.java
+++ src/com/Function/Function3.java
@@ -1,4 +1,32 @@
 package com.Function;
 
 public class Function3 {
+
+
+    public static void main(String[] args) {
+
+        new Print3().printSymbol();
+
+    }
 }
+
+final class Print3 {
+    private final String string = "* ";
+    public void printSymbol() {
+
+        int i = 1;
+
+        while (i <= 11) {
+
+
+            System.out.println(string.repeat(i));
+            System.out.println();
+            i += 1;
+
+        }
+
+    }
+
+}
+
+// task 14.3
