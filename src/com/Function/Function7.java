9046ca92b8b9d9874335b1b20e95225441da45d6
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-22 19:43:49 +0500

    ready task 14.7

--- src/com/Function/Function7.java
+++ src/com/Function/Function7.java
@@ -1,4 +1,31 @@
 package com.Function;
 
 public class Function7 {
+
+
+    public static void main(String[] args) {
+
+        new Print7().Exchange();
+
+    }
+}
+
+class Print7 {
+
+    public void Exchange() {
+
+        int a = 1;
+        int b;
+        int c = 3;
+        int d;
+
+        b = a;
+        d = c;
+
+        System.out.println(b + " " + d);
+
+        // Print 1,3
+
+
+    }
 }
