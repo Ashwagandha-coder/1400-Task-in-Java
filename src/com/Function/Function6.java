eef4a58214116101205212c959095919aab7cbc3
Ashwagandha-coder <max.cacz@yandex.ru>
2022-05-22 19:37:05 +0500

    ready task 14.6

--- src/com/Function/Function6.java
+++ src/com/Function/Function6.java
@@ -1,4 +1,33 @@
 package com.Function;
 
 public class Function6 {
+
+
+    public static void main(String[] args) {
+
+        new Print6().printSymbol6();
+
+    }
+}
+
+final class Print6 {
+
+    private final String STRING_1 = "*";
+    private final String STRING_2 = " ";
+
+    public void printSymbol6() {
+
+        System.out.println(STRING_1);
+
+        for (int i = 0; i <= 6; i++) {
+
+            System.out.println(STRING_1 + STRING_2.repeat(i) + STRING_1);
+
+        }
+        System.out.print(STRING_1.repeat(9));
+
+    }
+
+
+
 }
