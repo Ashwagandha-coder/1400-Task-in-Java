1e449fdc0e1d3fd54d44f554e62840d88ce8d864
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-01 22:39:30 +0500

    ready task 3.4

--- src/com/Arifmetics/Arifmatics4.java
+++ src/com/Arifmetics/Arifmatics4.java
@@ -1,4 +1,31 @@
 package com.Arifmetics;
 
+import java.util.Scanner;
+
 public class Arifmatics4 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new Apple().DelenieApple(scanner.nextInt(), scanner.nextInt());
+
+    }
 }
+
+class Apple {
+
+    public void DelenieApple(int N, int k) {
+
+        int result = k / N;
+        int backet = k % N;
+
+        System.out.println(result);
+        System.out.println(backet);
+
+
+    }
+
+
+}
+
+// task 3.4
