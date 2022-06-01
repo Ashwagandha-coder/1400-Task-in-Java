9dffb8458240479ecb0504a463b77a078b28b8ef
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-01 23:04:19 +0500

    ready task 3.5

--- src/com/Arifmetics/Arifmatics5.java
+++ src/com/Arifmetics/Arifmatics5.java
@@ -1,4 +1,19 @@
 package com.Arifmetics;
 
 public class Arifmatics5 {
+
+    public static void main(String[] args) {
+
+        new Square().CutSquare(543);
+
+    }
 }
+
+class Square {
+
+    public void CutSquare(int size) { System.out.println(size / 130); }
+
+
+}
+
+// task 3.5
