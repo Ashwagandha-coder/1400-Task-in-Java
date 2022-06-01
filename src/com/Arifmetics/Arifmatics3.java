761253dc2bd91fb95817f49beb71634520a68444
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-01 22:25:34 +0500

    ready task 3.3

--- src/com/Arifmetics/Arifmatics3.java
+++ src/com/Arifmetics/Arifmatics3.java
@@ -1,4 +1,25 @@
 package com.Arifmetics;
 
 public class Arifmatics3 {
+
+
+    public static void main(String[] args) {
+
+        new Date().PaymentWeek(234);
+
+    }
 }
+
+class Date {
+
+    public void PaymentWeek(int days) {
+
+        System.out.println(days / 7);
+
+    }
+
+
+
+}
+
+// task 3.3
