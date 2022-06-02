ccf77abbd0875fa3a3bcdeda1eca2aba95f848df
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-02 13:50:14 +0500

    ready task 3.8

--- src/com/Arifmetics/Arifmatics8.java
+++ src/com/Arifmetics/Arifmatics8.java
@@ -1,4 +1,31 @@
 package com.Arifmetics;
 
+import java.util.Scanner;
+
 public class Arifmatics8 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new PaymentPlace().TestPaymentPlace(scanner.nextDouble());
+
+    }
 }
+
+class PaymentPlace {
+
+    public void TestPaymentPlace(double x) {
+
+        if (x % 15 == 0)
+            System.out.println((int) x / 15);
+        else
+            System.out.println((int) x / 15 + 1);
+
+
+
+    }
+
+
+}
+
+// task 3.8
