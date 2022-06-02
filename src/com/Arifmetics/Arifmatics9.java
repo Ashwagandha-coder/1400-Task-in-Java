bdc085b5319b2384fd1e6b735533ae2509ac8ec2
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-02 13:56:33 +0500

    ready task 3.9

--- src/com/Arifmetics/Arifmatics9.java
+++ src/com/Arifmetics/Arifmatics9.java
@@ -1,4 +1,28 @@
 package com.Arifmetics;
 
+import java.util.Scanner;
+
 public class Arifmatics9 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new PaymentTime().TestPaymentTime(scanner.nextInt());
+
+    }
 }
+
+class PaymentTime {
+
+    public void TestPaymentTime(int x) {
+
+        System.out.println(x / 3600);
+
+
+
+    }
+
+
+}
+
+// task 3.9
