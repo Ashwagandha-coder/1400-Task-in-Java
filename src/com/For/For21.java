32caff8c6d910d5a2ce368260e118ef552426d1e
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-12 22:37:42 +0500

    ready task 5.21 and test

--- src/com/For/For21.java
+++ src/com/For/For21.java
@@ -1,4 +1,27 @@
 package com.For;
 
+import java.util.Scanner;
+
 public class For21 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new Payment21().paymentValue21(scanner.nextInt());
+
+    }
+}
+
+class Payment21 {
+
+    public void paymentValue21(int kilograms) {
+
+        for (int i = 50; i <= 1000; i += 50) {
+
+            System.out.println((double) kilograms * i / 1000);
+
+        }
+
+    }
+
 }
---
+++ src/com/Junit/Payment21Test.java
@@ -0,0 +1,15 @@
+package com.Junit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Payment21Test {
+
+    @Test
+    void paymentValue21() {
+
+
+
+    }
+}
