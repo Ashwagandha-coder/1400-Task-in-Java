8e9b6f44d23c97a0ef7c3a4b7bd90c033f6fd9c5
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-12 22:01:16 +0500

    ready task 5.17

--- src/com/For/For17.java
+++ src/com/For/For17.java
@@ -1,4 +1,30 @@
 package com.For;
 
 public class For17 {
+
+
+    public static void main(String[] args) {
+
+        new Payment17().paymentValue17();
+
+    }
+}
+
+class Payment17 {
+
+    public void paymentValue17() {
+
+        for (int i = 4; i <= 28; i++) {
+
+            int t = i + 3;
+            double y = 3 * Math.pow(t,2) + 4.87 * t - 3;
+            System.out.println(y);
+
+
+        }
+
+
+    }
+
+
 }
