e1b0c460e2f50e3d0b3bfe9c8af8f7f460b21259
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-12 22:07:24 +0500

    ready task 5.18

--- src/com/For/For18.java
+++ src/com/For/For18.java
@@ -1,4 +1,29 @@
 package com.For;
 
-public class For18 {
+public class For18 implements Payment18 {
+
+
+    public static void main(String[] args) {
+
+       new For18().paymentValue18();
+
+    }
+}
+
+interface Payment18 {
+
+    default void paymentValue18() {
+
+        for (int i = 2; i <= 17; i++) {
+
+            int t = 3 * i;
+            double z = 4.3 * Math.pow(t,2) - 8 * t + 13;
+            System.out.println(z);
+
+
+        }
+
+
+    }
+
 }
