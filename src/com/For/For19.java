defb4de4cfadd64bd1f23886a75b8bea9abade08
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-12 22:11:02 +0500

    ready task 5.19

--- src/com/For/For19.java
+++ src/com/For/For19.java
@@ -1,4 +1,27 @@
 package com.For;
 
 public class For19 {
+
+
+    public static void main(String[] args) {
+
+        new Payment19().paymentValue19();
+
+    }
+}
+
+class Payment19 {
+
+    public void paymentValue19() {
+
+        for (double i = 0.1; i <= 1.5; i += 0.1) {
+
+            System.out.println(Math.sin(i));
+
+        }
+
+
+    }
+
+
 }
