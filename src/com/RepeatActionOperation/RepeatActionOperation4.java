bd5a4f6dd73c49f96c20246cdecc2b490b709304
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-13 14:11:10 +0500

    ready task 6.4

--- src/com/RepeatActionOperation/RepeatActionOperation4.java
+++ src/com/RepeatActionOperation/RepeatActionOperation4.java
@@ -1,4 +1,47 @@
 package com.RepeatActionOperation;
 
 public class RepeatActionOperation4 {
+
+
+    public static void main(String[] args) {
+
+        new RepeatAction4();
+
+    }
+}
+
+class RepeatAction4 {
+
+    public RepeatAction4() {
+        FunRepeatAction4 funRepeatAction4 = () -> {
+
+            int i = 189;
+
+            while (true) {
+
+                i++;
+                if (i % 17 == 0) {
+                    System.out.println(i);
+                    break;
+                }
+
+            }
+
+
+        };
+        funRepeatAction4.repeatActionOpr4(); }
+
+
+
+
+
+
+}
+
+@FunctionalInterface
+interface FunRepeatAction4 {
+
+    void repeatActionOpr4();
+
+
 }
