95b1ddc896853a5945449dffb0776d1057fc5963
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-13 14:22:01 +0500

    ready task 6.5

--- src/com/RepeatActionOperation/RepeatActionOperation5.java
+++ src/com/RepeatActionOperation/RepeatActionOperation5.java
@@ -1,4 +1,37 @@
 package com.RepeatActionOperation;
 
 public class RepeatActionOperation5 {
+
+
+    public static void main(String[] args) {
+
+        new RepeatAction5().repeatActionOpr5();
+
+    }
+}
+
+class RepeatAction5 implements InterRepeatAction5 {
+
+    @Override
+    public void repeatActionOpr5() {
+
+        for (int i = 5000; i >= 0; i--) {
+
+            if (i % 139 == 0) {
+
+                System.out.println(i);
+                break;
+
+            }
+
+        }
+
+    }
+}
+
+
+interface InterRepeatAction5 {
+
+    void repeatActionOpr5();
+
 }
