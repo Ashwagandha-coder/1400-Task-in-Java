6c4ce7e30c430a367fc44fce29cfdd43b7894c5b
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-13 13:43:18 +0500

    ready task 6.1

--- src/com/RepeatActionOperation/RepeatActionOperation.java
+++ src/com/RepeatActionOperation/RepeatActionOperation.java
@@ -1,4 +1,47 @@
 package com.RepeatActionOperation;
 
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
 public class RepeatActionOperation {
+
+
+    public static void main(String[] args) {
+
+        new RepeatAction().repeatActionOpr();
+
+    }
+}
+
+class ScannerVal {
+
+    public int[] scannerValue() {
+
+        Scanner scanner = new Scanner(in);
+
+        int[] value = new int[2];
+        value[0] = scanner.nextInt();
+        value[1] = scanner.nextInt();
+
+        return value;
+
+    }
+
+
+}
+class RepeatAction extends ScannerVal {
+
+    public void repeatActionOpr() {
+
+        int [] value;
+        value = scannerValue();
+
+        System.out.println(value[0] / value[1]);
+        System.out.println(value[0] % value[1]);
+
+
+    }
+
+
 }
