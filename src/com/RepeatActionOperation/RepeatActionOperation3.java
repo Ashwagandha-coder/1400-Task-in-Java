fb66acf27e0833d4f3c78adacd0c9e1d72aa78d1
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-13 14:03:48 +0500

    ready task 6.3

--- src/com/RepeatActionOperation/RepeatActionOperation3.java
+++ src/com/RepeatActionOperation/RepeatActionOperation3.java
@@ -1,4 +1,37 @@
 package com.RepeatActionOperation;
 
 public class RepeatActionOperation3 {
+
+
+    public static void main(String[] args) {
+
+        new RepeatAction3();
+
+    }
+}
+
+
+class RepeatAction3 {
+
+    private final FunRepeatAction3 funReapeatAction3 = () -> {
+
+        for (int i = 10; i <= 100; i++) {
+
+            if (i % 2 == 0)
+                System.out.println(i);
+
+        }
+
+    };
+
+    public RepeatAction3() { funReapeatAction3.repeatActionOpr3(); }
+
+
+}
+
+@FunctionalInterface
+interface FunRepeatAction3 {
+
+    void repeatActionOpr3();
+
 }
