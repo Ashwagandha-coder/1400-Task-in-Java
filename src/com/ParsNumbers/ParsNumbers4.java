8e9a218155b1b0e6eaafdfc5bdb448fd80b805ac
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-17 10:59:17 +0500

    ready task 8.4

--- src/com/ParsNumbers/ParsNumbers4.java
+++ src/com/ParsNumbers/ParsNumbers4.java
@@ -1,4 +1,52 @@
 package com.ParsNumbers;
 
+import java.util.Scanner;
+
 public class ParsNumbers4 {
+
+
+    public static void main(String[] args) {
+
+        new FirstNumberForCheckValue();
+
+    }
+}
+
+class FirstNumberForCheckValue {
+
+    private final CheckValue4 checkValue4 = () -> {
+
+        for (double i = 1; i <= 10; i++) {
+
+            if (1 / i < this.value)
+                System.out.println(1 / i);
+            break;
+
+        }
+
+    };
+
+
+    private double value;
+
+    public FirstNumberForCheckValue() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.value = scanner.nextDouble();
+
+        checkValue4.checkValueFirst();
+
+
+    }
+
+}
+
+@FunctionalInterface
+interface CheckValue4 {
+
+
+    void checkValueFirst();
+
+
 }
