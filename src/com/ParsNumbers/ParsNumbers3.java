15fb808e38ea9357211b26ae586683da84f0fb00
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-17 10:49:45 +0500

    ready task 8.3

--- src/com/ParsNumbers/ParsNumbers3.java
+++ src/com/ParsNumbers/ParsNumbers3.java
@@ -1,4 +1,29 @@
 package com.ParsNumbers;
 
+import java.util.Scanner;
+
 public class ParsNumbers3 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new PrintForCheckValue().check(scanner.nextDouble());
+
+    }
+}
+
+class PrintForCheckValue {
+
+    public void check(double value) {
+
+        for (double i = 1; i <= 10; i++) {
+
+            if ((double) 1 / i < value)
+                System.out.println((double) 1 / i);
+
+        }
+
+
+    }
+
 }
