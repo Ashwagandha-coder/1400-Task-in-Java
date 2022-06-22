6526b562123574774edb60a6464041c1716f1b5c
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-22 11:52:22 +0500

    ready task 11.3

--- src/com/Massive/Massive3.java
+++ src/com/Massive/Massive3.java
@@ -1,5 +1,44 @@
 package com.Massive;
 
+import java.util.Arrays;
+
 public class Massive3 {
+
+
+    public static void main(String[] args) {
+
+        WriteInMassive writeInMassive = new WriteInMassive();
+        writeInMassive.writeValueMassive();
+        writeInMassive.printValue();
+
+
+    }
+
+}
+
+final class WriteInMassive {
+
+    private double[] massive;
+
+    public void writeValueMassive() {
+
+        massive = new double[15];
+
+        for (int i = 0; i <= 14; i++) {
+
+            massive[i] = Math.random();
+
+        }
+
+
+    }
+
+    public void printValue() {
+
+        System.out.println(Arrays.toString(massive));
+
+
+    }
+
 
 }
