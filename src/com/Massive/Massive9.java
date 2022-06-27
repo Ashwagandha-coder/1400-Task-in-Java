1fa15abc7572eca6bdc606376699f9143fe17590
Ashwagandha-coder <max.cacz@yandex.ru>
2022-06-27 23:52:44 +0500

    ready task 11.9

--- src/com/Massive/Massive9.java
+++ src/com/Massive/Massive9.java
@@ -1,5 +1,56 @@
 package com.Massive;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Massive9 {
+
+
+    public static void main(String[] args) {
+
+        ViceVersaMassive viceVersaMassive = new ViceVersaMassive();
+        viceVersaMassive.initMassive();
+        viceVersaMassive.viceVersaPrintMassive();
+
+    }
+
+}
+
+final class ViceVersaMassive {
+
+    private int[] massive;
+
+    public void initMassive() {
+
+        massive = new int[6];
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i <= 5; i++) {
+
+            massive[i] = scanner.nextInt();
+
+        }
+
+
+    }
+
+    public void viceVersaPrintMassive() {
+
+        int length_massive = massive.length - 1;
+
+        int[] newMassive = new int[massive.length];
+
+        for (int i = 0; i <= massive.length - 1; i++) {
+
+            newMassive[i] = massive[length_massive - i];
+
+        }
+
+        System.out.println(Arrays.toString(newMassive));
+
+
+    }
+
 
 }
