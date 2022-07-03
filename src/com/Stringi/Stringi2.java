ae99e13d311bf5faa01c91afbe42c2c7194d3dc0
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-03 12:06:26 +0500

    done task 12.2

--- src/com/Stringi/Stringi2.java
+++ src/com/Stringi/Stringi2.java
@@ -1,4 +1,46 @@
 package com.Stringi;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Stringi2 {
+
+
+    public static void main(String[] args) {
+
+        QueryValues queryValues = new QueryValues();
+        queryValues.init();
+        queryValues.query();
+
+    }
+}
+
+class QueryValues {
+
+    private String city;
+    private String country;
+
+
+    public void init() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        this.country = scanner.nextLine();
+        this.city = scanner.nextLine();
+
+
+    }
+
+    public void query() {
+
+        PrintStream printStream = new PrintStream(System.out,true, StandardCharsets.UTF_8);
+
+        printStream.println("Столица государства" + " " + country + " " + "город" + " " + city);
+
+
+
+    }
+
+
 }
