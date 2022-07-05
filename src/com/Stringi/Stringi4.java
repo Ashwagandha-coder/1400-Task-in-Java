b00641674c6e08e7a8a96481b1dc7ef044ee5650
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-05 23:54:55 +0500

    init task

--- src/com/Stringi/Stringi4.java
+++ src/com/Stringi/Stringi4.java
@@ -1,4 +1,18 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi4 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+
+
+        String f1 = str1;
+        String f2 = str2;
+    }
 }
