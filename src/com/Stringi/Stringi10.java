e6b4a03077dacb964bef7ccf2ac35734b9918398
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-15 13:16:19 +0500

    done task 12.10

--- src/com/Stringi/Stringi10.java
+++ src/com/Stringi/Stringi10.java
@@ -1,4 +1,37 @@
 package com.Stringi;
 
 public class Stringi10 {
+
+
+    public static void main(String[] args) {
+
+        new ReferenceValue().doSome3();
+
+    }
+}
+
+class ReferenceValue {
+
+    public void doSome3() {
+
+        String a = "qu";
+        String b = "qc";
+        String c = "du";
+
+        b = c;
+        a = b;
+        c = a;
+
+        b = a;
+        c = b;
+        a = c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
+
 }
