8d87a22aa467a3ac43a06565e3be6c2ccb8a99b7
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-15 13:01:55 +0500

    done task 12.8

--- src/com/Stringi/Stringi8.java
+++ src/com/Stringi/Stringi8.java
@@ -1,4 +1,45 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi8 {
+
+
+    public static void main(String[] args) {
+
+        CityName3 cityName3 = new CityName3();
+        cityName3.scannerCityName();
+        cityName3.cht3();
+
+    }
+}
+
+class CityName3 {
+
+    private String city1;
+    private String city2;
+    private String city3;
+
+    public void scannerCityName() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        city1 = scanner.nextLine();
+        city2 = scanner.nextLine();
+        city3 = scanner.nextLine();
+
+
+    }
+
+
+    public void cht3() {
+
+        int count1 = city1.length();
+        int count2 = city2.length();
+        int count3 = city3.length();
+
+        System.out.println(Math.max(Math.max(count1,count2), count3));
+        System.out.println(Math.min(Math.min(count1,count2), count3));
+    }
+
 }
