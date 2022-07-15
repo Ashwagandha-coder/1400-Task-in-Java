34d6827090ebe5f625230fb100c7572dcbca5e32
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-15 13:10:29 +0500

    done task 12.9

--- src/com/Stringi/Stringi9.java
+++ src/com/Stringi/Stringi9.java
@@ -1,4 +1,36 @@
 package com.Stringi;
 
 public class Stringi9 {
+
+
+    public static void main(String[] args) {
+
+        new ReferenceVariable().doSome();
+
+    }
+}
+
+class ReferenceVariable {
+
+    public void doSome() {
+
+        String country_1 = "Russia";
+        String country_2 = "USA";
+
+        String s1;
+        String s2;
+
+        s1 = country_1;
+        s2 = country_2;
+
+        s1 = s2;
+        s2 = s1;
+
+        System.out.println(s1);
+        System.out.println(s2);
+
+
+    }
+
+
 }
