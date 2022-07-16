f94555cd8d89b9f562d2bded9cedab85ba87a58a
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 14:50:54 +0500

    done task 12.20

--- src/com/Stringi/Stringi20.java
+++ src/com/Stringi/Stringi20.java
@@ -1,4 +1,39 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi20 {
+
+
+    public static void main(String[] args) {
+
+        PartWord partWord = new PartWord();
+
+        partWord.scannerWord();
+        partWord.printPartWord();
+
+    }
+}
+
+class PartWord {
+
+    private String word;
+
+    public void scannerWord() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+    }
+
+    public void printPartWord() {
+
+        int end = word.length() / 2;
+
+        System.out.println(word.substring(0,end));
+
+
+    }
+
+
 }
