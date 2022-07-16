49eb50098be736ec4f73c90fcaac123772e4bc26
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 14:35:50 +0500

    done task 12.17

--- src/com/Stringi/Stringi17.java
+++ src/com/Stringi/Stringi17.java
@@ -1,4 +1,38 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi17 {
+
+
+    public static void main(String[] args) {
+
+        SumSymbol symbol = new SumSymbol();
+
+        symbol.scannerWord();
+        symbol.sumSymbolWord();
+
+    }
+}
+
+class SumSymbol {
+
+    private String word;
+
+    public void scannerWord() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+
+    }
+
+    public void sumSymbolWord() {
+
+        System.out.print(word.charAt(1));
+        System.out.print(word.charAt(3));
+
+    }
+
+
 }
