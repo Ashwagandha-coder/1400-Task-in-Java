c125320b26eeaf899a64fd18c6dc861573adbf24
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 14:19:52 +0500

    done task 12.15

--- src/com/Stringi/Stringi15.java
+++ src/com/Stringi/Stringi15.java
@@ -1,4 +1,39 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi15 {
+
+
+    public static void main(String[] args) {
+
+        CompareLast compareLast = new CompareLast();
+
+        compareLast.scannerSymbolLast();
+        compareLast.compareSymbolLast();
+
+    }
+}
+
+class CompareLast {
+
+    private String word;
+
+    public void scannerSymbolLast() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+
+    }
+
+    public void compareSymbolLast() {
+
+        if (word.charAt(0) == word.charAt(word.length() - 1))
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+
+
+    }
 }
