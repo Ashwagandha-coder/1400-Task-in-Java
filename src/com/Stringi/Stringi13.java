7f37014331aac7305ebc900ef570e76159ba0098
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 14:04:46 +0500

    done task 12.13

--- src/com/Stringi/Stringi13.java
+++ src/com/Stringi/Stringi13.java
@@ -1,4 +1,40 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi13 {
+
+
+    public static void main(String[] args) {
+
+        OprSymbol oprSymbol = new OprSymbol();
+
+        oprSymbol.scannerSymbol();
+        oprSymbol.printSymbol();
+
+    }
+}
+
+class OprSymbol {
+
+    private String word;
+    private int k;
+
+    public void scannerSymbol() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+        k = scanner.nextInt();
+
+    }
+
+
+    public void printSymbol() {
+
+        System.out.println(word.charAt(k));
+
+    }
+
+
 }
