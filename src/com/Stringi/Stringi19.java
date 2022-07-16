26b1f9173ec5d509bbbda401c57cdbec73bf22d7
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 14:41:46 +0500

    done task 12.19

--- src/com/Stringi/Stringi19.java
+++ src/com/Stringi/Stringi19.java
@@ -1,4 +1,39 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi19 {
+
+
+    public static void main(String[] args) {
+
+        ThreeSymbol threeSymbol = new ThreeSymbol();
+
+        threeSymbol.scannerWord();
+        threeSymbol.sumSymbolWord();
+
+    }
+}
+
+class ThreeSymbol {
+
+    private String word;
+
+    public void scannerWord() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+
+    }
+
+    public void sumSymbolWord() {
+
+        System.out.print(word.charAt(1));
+        System.out.print(word.charAt(2));
+        System.out.print(word.charAt(3));
+
+    }
+
+
 }
