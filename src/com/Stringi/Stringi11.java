00d6bbdf1bc2163a725f8733def618e159c16049
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-16 13:21:39 +0500

    done task 12.11

--- src/com/Stringi/Stringi11.java
+++ src/com/Stringi/Stringi11.java
@@ -1,4 +1,37 @@
 package com.Stringi;
 
+import java.util.Scanner;
+
 public class Stringi11 {
+
+
+    public static void main(String[] args) {
+
+        Symbol symbol = new Symbol();
+
+        symbol.scannerWord();
+        symbol.printSymbol();
+
+    }
+}
+
+class Symbol {
+
+    private String word;
+
+    public void scannerWord() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        word = scanner.nextLine();
+
+    }
+
+    public void printSymbol() {
+
+        System.out.println(word.charAt(2));
+
+
+    }
+
 }
