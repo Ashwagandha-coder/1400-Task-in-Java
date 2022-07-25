9e95e2c7be18bf65132571c32fabb353d181866c
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-25 23:18:14 +0500

    done task 10.14

--- src/com/Massive/Massive14.java
+++ src/com/Massive/Massive14.java
@@ -1,5 +1,35 @@
 package com.Massive;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Massive14 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        new FullingMassive().foo(scanner.nextInt());
+
+    }
+
+}
+
+class FullingMassive {
+
+    public void foo(int n) {
+
+        int[] massive = new int[n];
+
+        int j = 0;
+
+        for (int i = n; i >= 1; i--) {
+
+            massive[j] = i;
+            j++;
+
+        }
+
+        System.out.println(Arrays.toString(massive));
+    }
 
 }
