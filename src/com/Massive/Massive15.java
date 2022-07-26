563cdc7e640c093718dccb09e83c1549d628f450
Ashwagandha-coder <max.cacz@yandex.ru>
2022-07-27 00:05:31 +0500

    done task 10.15

--- src/com/Massive/Massive15.java
+++ src/com/Massive/Massive15.java
@@ -1,5 +1,43 @@
 package com.Massive;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Massive15 {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] massive = new int[8];
+
+        for (int i = 0; i<8; i++) {
+
+            massive[i] = scanner.nextInt();
+
+        }
+
+        new SomeClass(massive).doSome();
+
+    }
+
+}
+
+class SomeClass {
+
+    private int[] massive;
+
+    public SomeClass(int... elem) {
+        massive = elem;
+    }
+
+    public void doSome() {
+
+
+        Arrays.sort(massive);
+
+
+        System.out.println(Arrays.toString(massive));
+
+
+    }
 }
